c6c2eb7bb18dfa602762fb6e2765f1404d00eb04
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-18 13:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,18 +8,23 @@ class Formatter {
   }
   static titleize(name) {
     let except = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
-    let newString = [];
     let splitStr = name.toLowerCase().split(' ');
 
     for ( let i = 0; i < splitStr.length; i++ ) {
-      if ( except.includes(splitStr[i])) {
-        splitStr[i] = splitStr[i].charAt(0).toLowerCase() + splitStr[i].substring(1);
+      if (i == 0) {
+        name.charAt(0).toUpperCase() + name.slice(1)
       } else {
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        
       }
-
     }
 
     return splitStr.join( " " );
   }
 }
+
+
+// if ( except.includes(splitStr[i])) {
+//   splitStr[i] = splitStr[i].charAt(0).toLowerCase() + splitStr[i].substring(1);
+// } else {
+//   splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+// }
