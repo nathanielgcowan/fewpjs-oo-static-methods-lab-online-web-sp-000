20f2b7cd137c63f0582adc54c8beba33f90dfb47
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-18 13:18:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Formatter {
 
     for ( let i = 0; i < splitStr.length; i++ ) {
       if ( i == 0 ) {
-        newString.push( this.capitalize( splitStr[ n ] ) )
+        newString.push( this.capitalize( splitStr[ i ] ) )
       } else {
         if ( except.includes( splitStr[ i ] ) ) {
           newString.push( splitStr[ i ] )
