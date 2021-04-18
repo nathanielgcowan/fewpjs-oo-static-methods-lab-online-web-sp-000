746f2a159c86753a8fdef293bf35f2e490d5c211
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-18 12:54:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class Formatter {
   }
   static titleize(name) {
     let except = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
-    
+
     let splitName = name.toLowerCase().split(' ');
     for (let i = 0; i < splitName.length; i++) {
         splitName[i] = splitName[i].charAt(0).toUpperCase() + splitName[i].substring(1);
