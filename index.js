63802070f70787893d182a086691b4380e678eea
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-18 12:51:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ class Formatter {
   static sanitize(name) {
     return name.replace(/[^-,'A-Za-z0-9 ]+/g, '');
   }
-  static titleize(name) {
-    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by',  'from']
-
-    return name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
-
+  static titleize(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        // You do not need to check if i is larger than splitStr length, as your for does that for you
+        // Assign it back to the array
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    // Directly return the joined string
+    return splitStr.join(' '); 
   }
 }
