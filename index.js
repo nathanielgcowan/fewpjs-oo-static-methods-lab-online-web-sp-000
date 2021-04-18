d1a4a0622967f49a4378121a13b326a1b088c712
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-18 13:26:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Formatter {
 
     for ( let i = 0; i < splitStr.length; i++ ) {
       if ( i == except ) {
-        newString.push( this.capitalize( splitStr[ i ] ) )
+        splitStr
       } else {
         splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
       }
