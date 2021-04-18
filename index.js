2316829a655df2b531af8a7e05902ae59bf9347b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-18 12:48:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1)
+  }
+  static sanitize(name) {
+    return name.replace(/[^-,'A-Za-z0-9 ]+/g, '');
+  }
+  static titleize(name) {
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by',  'from']
+
+    return name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
+
+  }
 }
